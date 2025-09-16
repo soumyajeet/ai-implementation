0cabdbf8f209f04690a59a6bf3a71715569faea8
Soumyajit <sendtosoumyajit@gmail.com>
2025-09-16 23:01:00 +0530

    Add product FAQ and implement RAG demo
    - Created `product-data.txt` to include a comprehensive FAQ database covering products, orders, shipping, returns, and account management.
    - Developed `simple_rag_demo.py` to demonstrate a retrieval-augmented generation (RAG) setup using Langchain, integrating the FAQ data for question answering.

---
+++ RAG/simple_rag_demo.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+llm = ChatOpenAI(model="gpt-4o-mini", api_key=OPENAI_API_KEY)
+
+document = TextLoader("g:/SANDBOX/backend-as-service/RAG/product-data.txt").load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+chunk = text_splitter.split_documents(document)
+store = Chroma.from_documents(chunk, embeddings)
+retriever = store.as_retriever()
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system","""You are a helpful assistant for answering questions. Use the provided context to respond. 
+        If the answer in unclear respond with the message I don't know. Answer the question in three consice lines.
+        {context}"""),
+        ("human","{input}")
+    ]
+)
+
+qa_chain = create_stuff_documents_chain(llm, prompt_template)
+rag_chain = create_retrieval_chain(retriever, qa_chain) 
+
+print("Chain with document")
+question = input("Enter your question: ")
+
+if question:
+    response = rag_chain.invoke({"input": question})
+    print(response['answer'])
+
